0042c6eacfe22db411a375dc838bc63f2f01cb4d
Shohruxshoh <shohhhhhhhhhhhhhhhhhhhh@gmail.com>
2023-12-25 21:26:58 +0500

    a

--- carBooking/urls.py
+++ carBooking/urls.py
@@ -24,6 +24,9 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 schema_view = get_schema_view(
     openapi.Info(
         title="Snippets API",
@@ -34,7 +37,7 @@ schema_view = get_schema_view(
         license=openapi.License(name="BSD License"),
     ),
     public=True,
-    permission_classes=(permissions.AllowAny,),
+    permission_classes=(permissions.AllowAny, )
 )
 
 urlpatterns = [
@@ -51,3 +54,7 @@ urlpatterns += [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+if settings.DEBUG:
+    urlpatterns += (
+        static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+        )
