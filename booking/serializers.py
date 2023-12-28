fe6c94a04a15761b84725d72cd9ea05499f050ee
Shohruxshoh <shohhhhhhhhhhhhhhhhhhhh@gmail.com>
2023-12-28 14:26:28 +0500

    b

--- booking/serializers.py
+++ booking/serializers.py
@@ -42,7 +42,7 @@ class BookingSerializer(serializers.ModelSerializer):
         date1 = attrs.get('date')
         start = attrs.get('start')
         end = attrs.get('end')
-        booking_day = Booking.objects.filter(car=car, parking=parking, date=date1).first()
+        booking_day = Booking.objects.filter(car=car, parking=parking, date=date1)
         booking = Booking.objects.filter(car=car, is_parking=True)
         booked_slot = Booking.objects.filter(parking=parking, date=date1, start__range=[start, end],
                                              end__range=[start, end], is_parking=True)
@@ -53,6 +53,8 @@ class BookingSerializer(serializers.ModelSerializer):
         if booked_slot.exists():
             raise ValidationError({"success": False, "message": "Bu joy band siz qo'ymoqchi bo'lgan vaqtda!"})
 
+        print(self.is_booking_day_valid(booking_day, start, end))
+
         if booking_day and self.is_booking_day_valid(booking_day, start, end):
             raise ValidationError({"success": False, "message": "Bir kunda 3 soatdan ko'p foydalanish mumkin emas!"})
 
@@ -62,10 +64,14 @@ class BookingSerializer(serializers.ModelSerializer):
     def is_booking_day_valid(booking_day, start, end):
         start_datetime = datetime.combine(date.today(), start)
         end_datetime = datetime.combine(date.today(), end)
-        start_datetime1 = datetime.combine(date.today(), booking_day.start)
-        end_datetime1 = datetime.combine(date.today(), booking_day.end)
+        time_difference1 = timedelta(hours=0)
+        for booking in booking_day:
+            start_datetime1 = datetime.combine(date.today(), booking.start)
+            end_datetime1 = datetime.combine(date.today(), booking.end)
+            time_difference1 += end_datetime1 - start_datetime1
+            print(time_difference1)
+
         time_difference = end_datetime - start_datetime
-        time_difference1 = end_datetime1 - start_datetime1
         time_difference2 = time_difference1 + time_difference
         return time_difference2 >= timedelta(hours=3)
 
